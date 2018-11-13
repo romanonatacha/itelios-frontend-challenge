46c9372d1151d1850fe4b2dc159f7884641de6dd
natacha romano <romano.n@hotmail.com>
2018-11-13 19:20:26 -0200

    setting gulp file and folders architecture

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,36 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var babel = require('gulp-babel');
+var serve = require('gulp-serve');
+var browserSync = require('browser-sync').create();
+var connect = require('gulp-connect');
+ 
+gulp.task('webserver', function() {
+  connect.server();
+});
 
-gulp.task('default', function() {
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./dist"
+        }
+    });
+});
 
+gulp.task('default', ['webserver', 'scripts', 'styles'], function() {
+    gulp.watch('js/*/*.js', ['scripts']);
+    gulp.watch('sass/*/*.scss', ['styles']);
+
+});
+
+gulp.task('scripts', function() {
+    gulp.src('js/**/*.js')
+    .pipe(babel())
+       .pipe(gulp.dest('dist/js'))
+   });
+
+gulp.task('styles', function() {
+ gulp.src('sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('dist/css'))
 });
