93749d52db1fb66e0ec9fc894ecd1c325b795c48
natacha romano <romano.n@hotmail.com>
2018-11-14 19:01:27 -0200

    final script

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ $.getJSON("./products.json", function (data) {
   var mainProduct = data[0].data.item;
   var mainProductTemplate = "<div class='main__item__product__image'> \
     <img src='"+ mainProduct.imageName + "'> \
-    <p class='main__item__product__image__name'>" +  mainProduct.name + "</p> \
+    <p class='main__item__product__image__name'>" + mainProduct.name + "</p> \
     <p class='main__item__product__image__price'>Por <strong>" + mainProduct.price + "</strong></p> \
     <p class='main__item__product__image__paymentConditions'>" + mainProduct.productInfo.paymentConditions + "</p></div>";
 
@@ -11,13 +11,13 @@ $.getJSON("./products.json", function (data) {
 
 
   var products = data[0].data.recommendation;
-    var recommendationList = '';
+  var recommendationList = '';
 
   for (var i = 0; i < products.length; i++) {
-      var product = products[i];
-      recommendationList += "<div class='main__interess__list__image swiper-slide'> \
+    var product = products[i];
+    recommendationList += "<div class='main__interess__list__image swiper-slide'> \
       <img src='"+ product.imageName + "' alt='" + product.name + "'> \
-       <p class='main__interess__list__image__name'>" +  product.name + "</p> \
+       <p class='main__interess__list__image__name'>" + product.name + "</p> \
        <p class='main__interess__list__image__price'>Por: <strong>" + product.price + "</strong></p> \
        <p class='main__interess__list__image__paymentConditions'>" + product.productInfo.paymentConditions + "</p> \
        <button class='main__interess__list__image__btn'>adicionar ao carrinho<i class='material-icons'>add_shopping_cart</i></button></div>";
@@ -26,4 +26,22 @@ $.getJSON("./products.json", function (data) {
 
 
   $('.swiper-wrapper').html(recommendationList);
+});
+
+
+$( document ).ready(function() {
+  
+  var swiper = new Swiper('.swiper-container', {
+    pagination: '.swiper-pagination', 
+    slidesPerView: 'auto',
+    paginationClickable: true,
+    spaceBetween: 0,
+    loop: true,
+    speed: 7500,
+    autoplay: {
+      enabled: true,
+      delay: 10000,
+      disableOnInteraction: true,
+    },
+  });
 });
