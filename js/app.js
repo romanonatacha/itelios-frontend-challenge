473f04a104b41f64322eba75cd4f7497d671dcfd
natacha romano <romano.n@hotmail.com>
2018-11-14 16:03:09 -0200

    json loading on the html

--- js/app.js
+++ js/app.js
@@ -4,27 +4,26 @@ $.getJSON("./products.json", function (data) {
   var mainProductTemplate = "<div class='main__item__product__image'> \
     <img src='"+ mainProduct.imageName + "'> \
     <p class='main__item__product__image__name'>" +  mainProduct.name + "</p> \
-    <p class='main__item__product__image__price'>" + mainProduct.price + "</p> \
+    <p class='main__item__product__image__price'>Por <strong>" + mainProduct.price + "</strong></p> \
     <p class='main__item__product__image__paymentConditions'>" + mainProduct.productInfo.paymentConditions + "</p></div>";
 
   $('.main__item__product').html(mainProductTemplate);
 
 
   var products = data[0].data.recommendation;
-  var recommendationList = '<div>';
+    var recommendationList = '';
 
   for (var i = 0; i < products.length; i++) {
       var product = products[i];
-      recommendationList += "<div class='main__interess__list__image'> \
-      <img src='"+ product.imageName + "'> \
+      recommendationList += "<div class='main__interess__list__image swiper-slide'> \
+      <img src='"+ product.imageName + "' alt='" + product.name + "'> \
        <p class='main__interess__list__image__name'>" +  product.name + "</p> \
-       <p class='main__interess__list__image__price'>" + product.price + "</p> \
+       <p class='main__interess__list__image__price'>Por: <strong>" + product.price + "</strong></p> \
        <p class='main__interess__list__image__paymentConditions'>" + product.productInfo.paymentConditions + "</p> \
-       <button class='main__interess__list__image__btn'>adicionar ao carrinho</button></div><div>";
-       recommendationList += "</div></div>";
+       <button class='main__interess__list__image__btn'>adicionar ao carrinho<i class='material-icons'>add_shopping_cart</i></button></div>";
 
   }
-  recommendationList += "</div>";
 
-  $('.main__interess__list').html(recommendationList);
+
+  $('.swiper-wrapper').html(recommendationList);
 });
